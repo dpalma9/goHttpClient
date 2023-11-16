43459b638d51bb95aab825fecef36b80e4f8e914
Dani <dani_workstation@IM1825.logicalis.local>
2023-11-16 16:19:15 +0000

    Add a simple http client

---
+++ README.md
@@ -0,0 +1,41 @@
+# Overview
+Golang HTTP TSL Client.
+
+# Run
+
+Build:
+
+```bash
+$ docker build -t gohttpclient -f Dockerfile .
+```
+And run:
+
+```bash
+$ docker run -d --rm --name goclient gohttpclient
+```
+
+# Dev with VSCode
+
+.devcontainer.json is a file that VSCode will read if you install the Dev Containers extensions.
+This file is used to pre-configure the VSCode with all the plugins you need on your dev container.
+
+# Develop (docker)
+
+In order to build your docker test image in go, you need to run it with the following command the first time:
+
+```Dockerfile
+# Run just the first time; then comment the line:
+RUN go mod init myclient && go mod tidy
+```
+
+Run your test container:
+
+```bash
+$ docker run -d --rm --name goclient gohttpclient
+```
+
+If you want to test your container using a localhost service, in order to the container was able to connect with localhost, you'll need to run the container on the host network:
+
+```bash
+$ docker run -d --rm --name goclient --network host gohttpclient
+```
---
+++ src/client.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	//"io/ioutil"
+	"log"
+)
+
+//var (
+//	certFile = flag.String("cert", "routeToSomeCertFile", "A PEM encoded certificate file.")
+//	keyFile  = flag.String("key", "routeToSomeKeyFile", "A PEM encoded private key file.")
+//	caFile   = flag.String("CA", "routeToSomeCertCAFile", "A PEM eoncoded CA's certificate file.")
+//	//service  = flag.String("url", "https://localhost:8080", "The service URL which the request will be made")
+//)
+//
+//func makeGetTLSRequest() {
+//	flag.Parse()
+//
+//	// Load client cert
+//	cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	// Load CA cert
+//	caCert, err := ioutil.ReadFile(*caFile)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	caCertPool := x509.NewCertPool()
+//	caCertPool.AppendCertsFromPEM(caCert)
+//
+//	// Setup HTTPS client
+//	tlsConfig := &tls.Config{
+//		Certificates: []tls.Certificate{cert},
+//		RootCAs:      caCertPool,
+//	}
+//	tlsConfig.BuildNameToCertificate()
+//	transport := &http.Transport{TLSClientConfig: tlsConfig}
+//	client := &http.Client{Transport: transport}
+//
+//	// Do GET something
+//	resp, err := client.Get("https://goldportugal.local:8443")
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	defer resp.Body.Close()
+//
+//	// Dump response
+//	data, err := ioutil.ReadAll(resp.Body)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	log.Println(string(data))
+//}
+//
+func makePostRequest(url string, body string) {
+
+	// Prepare a JSON body
+	bodyBytes, err := json.Marshal(&body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	reader := bytes.NewReader(bodyBytes)
+
+	// Make HTTP POST request
+	//resp, err := http.Post(url, "application/json", reader)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	// Make HTTP POST request (with Do)
+	request, err := http.NewRequest(http.MethodPost, url, reader)
+	if err != nil {
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	httpClient := &http.Client{}
+
+	resp, err := httpClient.Do(request)
+	if err != nil {
+		return
+	}
+
+	// Close response body
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Read response body
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.StatusCode >= 400 && resp.StatusCode <= 500 {
+		log.Println("Error response. Status Code: ", resp.StatusCode)
+	}
+
+	log.Println("Response:", string(responseBody))
+}
+
+func main() {
+	makePostRequest("http://0.0.0.0:9000", "{\"field1\": \"value1\"}")
+}
